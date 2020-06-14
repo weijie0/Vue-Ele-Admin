fd61ed37b4e758fee44afa33e0e98f919a00edf7
luanweijie <luanweijie@ronglianyiyun.com>
2020-06-14 17:09:23 +0800

    upload

---
+++ src/config/app.js
@@ -0,0 +1,9 @@
+import CommonConfig from "@/config/common";
+import SystemConfig from "@/config/system";
+
+export default {
+  ...CommonConfig,
+  ...SystemConfig,
+  corporation: "公司名教育科技（北京）有限公司",
+  siteName: "Vue-Ele-Admin管理后台"
+};
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  publicPath: "./",
+
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "",
+        changeOrigin: true
+      }
+    }
+  },
+  chainWebpack: config => {
+    // remove the prefetch plugin
+    config.plugins.delete("prefetch");
+  }
+};
